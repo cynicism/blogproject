7097a8fc2ba0db815d2808593e0b6b96ad0d0530
ni_zj@sina.cn <ni_zj@sina.cn>
2018-04-11 22:34:43 +0800

    my first commit

---
+++ blogproject/blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+# Create your views here.
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+'''    return render(request, 'blog/index.html', context={
+        'title': '我的博客首页',
+        'welcome': '欢迎访问我的博客首页'
+    })'''
+#    return HttpResponse("欢迎访问首页")
