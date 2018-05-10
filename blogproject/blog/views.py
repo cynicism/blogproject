c52bfd9d258291cb5da45b67fddb506af57627b6
ni_zj@sina.cn <ni_zj@sina.cn>
2018-05-10 23:02:58 +0800

    增加评论部分

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post, Category
+from comments.forms import CommentForm
 import markdown
 
 # Create your views here.
@@ -13,13 +14,20 @@ def index(request):
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.body = markdown.markdown(
-        post.body,extensions=[
+        post.body, extensions=[
             'markdown.extensions.extra',
             'markdown.extensions.codehilite',
             'markdown.extensions.toc',
         ]
     )
-    return render(request, 'blog/detail.html', context={'post': post})
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {
+        'post': post,
+        'form': form,
+        'comment_list': comment_list
+        }
+    return render(request, 'blog/detail.html', context=context)
 
 def archives(request, year, month):
     post_list = Post.objects.filter(created_time__year=year,
--- blogproject/comments/forms.py
+++ blogproject/comments/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 from .models import Comment
 
-class CommentForm(form.ModelForm):
+class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'url', 'text']
